5c39d29c5a28bb4f7672c55b1c9267da6ac89df2
Jamshed Ali <116933185+JamshedAli18@users.noreply.github.com>
2025-10-15 11:34:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # main_app.py
-import streamlit as st
-from sidebar import render_sidebar
-from chat_ui import render_chat_interface
-from session_manager import init_session_state
+from src.sidebar import render_sidebar
+from src.chat_ui import render_chat_interface
+from src.session_manager import init_session_state
+
 
 # ------------------- SESSION INIT -------------------
 init_session_state()
